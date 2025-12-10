1c9b7fd324ef367ae55f68b0bc5e3e802eb085a6
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2025-12-11 07:37:57 +0800

    [update] updated filenames in `view/`

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../view/Home.vue'
-import Package from '../view/Package.vue'
+import Home from '../view/HomeView.vue'
+import Package from '../view/PackageView.vue'
 
 const routes: RouteRecordRaw[] = [
   {
